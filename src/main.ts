9c459a8461381ca32a9a3caa0b8cc10c2e692fce
w3cj <cj@null.computer>
2024-10-25 12:12:36 -0600

    :sparkles: add caption dl and convert

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import env from "./env.ts";
-import { type Item, listLatestChannelVideos } from "./youtube.ts";
+import { join } from "@std/path";
+import { exists } from "@std/fs";
+import {
+  getVideoTranscript,
+  type Item,
+  listLatestChannelVideos,
+} from "./youtube.ts";
 
 if (import.meta.main) {
+  const dataPath = join("data");
   const kv = await Deno.openKv();
 
   for (const channelId of env.YOUTUBE_CHANNEL_IDS) {
@@ -29,6 +36,35 @@ if (import.meta.main) {
     console.log(channelId, "total new videos:", newVideos.length);
 
     const allVideos = newVideos.concat(existingVideos.value || []);
+
+    for (const video of allVideos) {
+      const { videoId } = video.id;
+      const videoPath = join(dataPath, videoId);
+      await Deno.mkdir(videoPath, {
+        recursive: true,
+      });
+
+      const captionsXMLPath = join(videoPath, "captions.xml");
+      const captionsSRTPath = join(videoPath, "captions.srt");
+      const captionsExist = (await Promise.all([
+        exists(captionsXMLPath),
+        exists(captionsSRTPath),
+      ])).every((v) => v);
+
+      if (!captionsExist) {
+        console.log("Getting transcript for:", videoId);
+        try {
+          const { srt, xml } = await getVideoTranscript(videoId);
+          await Deno.writeTextFile(captionsXMLPath, xml);
+          await Deno.writeTextFile(captionsSRTPath, srt);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      await new Promise((resolve) => {
+        setTimeout(resolve, 5000 + (Math.random() * 5000));
+      });
+    }
 
     await kv.set(videosKey, allVideos);
     await kv.set(lastVideoKey, allVideos[0].id.videoId);
--- src/youtube.ts
+++ src/youtube.ts
@@ -1,4 +1,6 @@
 import env from "./env.ts";
+import { DOMParser } from "jsr:@b-fuze/deno-dom";
+import { unescape } from "@std/html/entities";
 
 export interface YouTubeSearchResponse {
   kind: string;
@@ -76,4 +78,93 @@ export async function listLatestChannelVideos(
   const errorJson = (await response.json()) as YouTubeResponseError;
   console.error(errorJson);
   throw new Error(errorJson.error.message);
+}
+
+const requestHeaders = {
+  Host: "www.youtube.com",
+  "User-Agent":
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:132.0) Gecko/20100101 Firefox/132.0",
+  "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+  "Accept-Language": "en-US,en;q=0.5",
+  "Upgrade-Insecure-Requests": "1",
+  "Sec-Fetch-Dest": "document",
+  "Sec-Fetch-Mode": "navigate",
+  "Sec-Fetch-Site": "none",
+  "Sec-Fetch-User": "?1",
+};
+
+function secondsToTimestamp(totalSeconds: number) {
+  const milliseconds = Math.floor((totalSeconds % 1) * 1000);
+  totalSeconds = Math.floor(totalSeconds);
+  let remainingSeconds = totalSeconds;
+  const seconds = totalSeconds % 60;
+  remainingSeconds = totalSeconds - seconds;
+  const minutes = Math.floor((remainingSeconds / 60) % 60);
+  remainingSeconds = remainingSeconds - (minutes * 60);
+  const hours = Math.floor(remainingSeconds / 3600);
+
+  const pad = (value: number, amount = 2) =>
+    value.toString().padStart(amount, "0");
+
+  return `${pad(hours)}:${pad(minutes)}:${pad(seconds)},${
+    pad(milliseconds, 3)
+  }`;
+}
+
+export async function getVideoTranscript(videoId: string) {
+  const youtubeVideoUrl = `https://youtube.com/watch?v=${videoId}`;
+  const response = await fetch(youtubeVideoUrl, {
+    headers: requestHeaders,
+  });
+  const html = await response.text();
+  let captionsText = "";
+  try {
+    captionsText = html.split('"captions":')[1].split(',"videoDetails')[0];
+  } catch {
+    throw new Error(`No captions found: ${videoId}`);
+  }
+  const captionsMetaJSON = JSON.parse(captionsText);
+  const { baseUrl } =
+    captionsMetaJSON.playerCaptionsTracklistRenderer.captionTracks[0];
+  const captionsResponse = await fetch(baseUrl, {
+    headers: {
+      ...requestHeaders,
+      Referer: youtubeVideoUrl,
+    },
+  });
+  const xml = await captionsResponse.text();
+  const doc = new DOMParser().parseFromString(
+    xml,
+    "text/html",
+  );
+  const captions: {
+    start: number;
+    duration: number;
+    text: string;
+    end: number;
+  }[] = [];
+  doc.querySelectorAll("text").forEach((element) => {
+    const start = Number(element.getAttribute("start"));
+    const duration = Number(element.getAttribute("dur"));
+    const text = unescape(element.textContent);
+    captions.push({
+      start,
+      duration,
+      text,
+      end: start + duration,
+    });
+  });
+  let srt = "";
+  captions.forEach(({ start, end, text }, index) => {
+    const nextCaption = captions[index + 1];
+    const endSeconds = index < captions.length - 1 && nextCaption.start < end
+      ? nextCaption.start
+      : end;
+    srt += `${index + 1}
+${secondsToTimestamp(start)} --> ${secondsToTimestamp(endSeconds)}
+${text}
+
+`;
+  });
+  return { xml, srt };
 }
